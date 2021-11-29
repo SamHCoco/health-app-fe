4f93e5ff37683731716e274bb81a8ea3c191b369
Samuel Coco <Samuel.HenriAC@outlook.com>
2021-11-29 18:23:43 -0500

    Method for invoking user registration API added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route  } from 'react-router-dom';
 import RegistrationForm from './components/Registration';
+import RegistrationSuccess from './components/RegistrationSuccess';
 
 function App() {
   return (
     <div>
         <Route path="/sign-up" component={RegistrationForm}/>
+        <Route path="/registration-success" component={RegistrationSuccess} />
     </div>
   );
 }
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Form, Container } from 'semantic-ui-react';
 import axios from 'axios';
 
 function RegistrationForm() {
-  
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
 
+  const url = "http://localhost:8899/api/v1/user/register";
+
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    middleName: '',
+    email: '',
+    password: ''
+  });
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createUser();
+  };
+  
+  const createUser = async () => {
+    const payload = user;
+    console.log("User to be created: " + payload);
+    const {data : createdUser} = await axios.post(url, payload);
+    console.log(createdUser);
+  } 
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    console.log("Event is " + e.target.name) // todo - remove
+    setUser({...user, [e.target.name] : e.target.value});
+  }
 
   return (<Container>
-            <Form>
+            <Form handleSubmit={handleSubmit}>
               <Form.Group widths='equal'>
-                  <Form.Input fluid label='First name' placeholder='First name' />
-                  <Form.Input fluid label='Middle names' placeholder='(Optional)' />
-                  <Form.Input fluid label='Surname' placeholder='Surname' />
+                  <Form.Input fluid label='First name' placeholder='First name' value={user.firstName} name='firstName' onChange={handleChange} />
+                  <Form.Input fluid label='Middle names' placeholder='(Optional)' name='middleName' onChange={handleChange}/>
+                  <Form.Input fluid label='Surname' placeholder='Surname' value={user.lastName} name='lastName' onChange={handleChange} />
               </Form.Group>
               <Form.Field>
                   <label>Username</label>
-                  <input placeholder='Email' />
+                  <input placeholder='Email' type='email' value={user.email} name='email' onChange={handleChange} />
               </Form.Field>
               <Form.Field>
                   <label>Password</label>
-                  <input placeholder='Password' type='password' />
+                  <input placeholder='Password' type='password' value={user.password} name='password' onChange={handleChange} />
               </Form.Field>
-              <Button type='submit'>Sign up</Button>
+              <Button onClick={handleSubmit} type='submit'>Sign up</Button>
           </Form>
         </Container>);
 }
