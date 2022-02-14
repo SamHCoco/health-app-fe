47e056146f21240c0a2c15e23aa455e3a42e9c60
Samuel Coco <samuel.henriac@outlook.com>
2022-02-14 23:07:47 +0000

    feature/store product listing and filtering 14-02-22 23:07:43 WIP

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,29 @@
-import { Container, Segment, Card, Dropdown, Pagination } from "semantic-ui-react";
+import { Container, Segment, Card, Dropdown, Pagination, Grid } from "semantic-ui-react";
 import { React, useState, useEffect} from "react";
 
 import Product from "../components/Product";
 import httpService from "../service/httpService";
-import config from "../config/config.json";
-import DropdownMenu from './DropdownMenu';
+import config from '../config/config.json';
 
 function ProductList() {
   const [products, setProducts] = useState([{}]);
+  
   const [activePage, setActivePage] = useState(1);
+  const [apiUrl, setApiUrl] = useState(config.storeServiceUrl + '/product/paged/all?page=1&size=1');
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageSize, setPageSize] = useState(1);
   
   useEffect(() => {
     async function fetchProducts() {
-      const { data: productData } = await httpService.get(
-        config.storeServiceUrl + "/product/all"
-      );
-      console.log("Returned products: ");
-      setProducts(productData);
+      const { data: response } = await httpService.get(apiUrl);
+      console.log('Returned response: ');
+      console.log(response);
+      
+      setProducts(response.content);
+      setTotalPages(response.totalPages);
     }
     fetchProducts();
-  }, []);
+  },[apiUrl]);
 
 
   const sortOptions = [
@@ -27,34 +31,45 @@ function ProductList() {
         {key: 1, text: 'Price: highest to lowest', value: 'Price: highest to lowest'}
       ];
 
-  function handlePaginationChange(e, { activePage }) {
+  
+
+  function onPageChange(e, { activePage }) {
     setActivePage(activePage);
+    setApiUrl(config.storeServiceUrl + '/product/paged/all?page=' + activePage.toString() + '&size=1');
   }
   
   return (
-    <Segment>
-          <Container >
-            Sort By:
-            <Dropdown style = {{margin: 20}}
-              selectOnBlur={false}
-              selection
-              inline
-              placeholder={'sort by'}
-              options={sortOptions} />
-            <Card.Group itemsPerRow={5} >
-                {products.map(({id, name, manufacturer, price}) => <Product
-                                          key={id} 
-                                          name={name}
-                                          price={price}
-                                          manufacturer={manufacturer} />)}
-            </Card.Group>
-            <Pagination
-            activePage={activePage}
-            onPageChange={handlePaginationChange}
-            totalPages={5}
-          />
-        </Container>
-    </Segment>
+    <Grid centered>
+      {/* Sort By:
+              <Dropdown style = {{margin: 20}}
+                selectOnBlur={false}
+                selection
+                inline
+                placeholder={'sort by'}
+                options={sortOptions} /> */}
+        <Segment>
+            <Container >
+              <Card.Group itemsPerRow={5} >
+                  {products.map(({id, name, manufacturer, price}) => <Product
+                                            key={id} 
+                                            name={name}
+                                            price={price}
+                                            manufacturer={manufacturer} />)}
+              </Card.Group>
+              <Pagination
+                  activePage={activePage}
+                  boundaryRange={0}
+                  firstItem={null}
+                  lastItem={null}
+                  siblingRange={1}
+                  totalPages={5}
+                  ellipsisItem={null}
+                  defaultActivePage={1}
+                  onPageChange={onPageChange}
+              />
+          </Container>
+      </Segment>
+    </Grid>
   );
 }
 
