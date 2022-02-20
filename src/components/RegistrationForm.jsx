a24736995ddfd1d40386ecd0dc2e84ee23f56b36
Samuel Coco <samuel.henriac@outlook.com>
2022-02-20 18:05:05 +0000

    feature/user-registration 20/02/22 18:05:06 WIP

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
-import { Button, Form, Container } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Form, Container, Segment } from 'semantic-ui-react';
 import axios from 'axios';
+import { Redirect } from 'react-router';
+import RegistrationSuccess from './RegistrationSuccess';
 
 function RegistrationForm() {
 
   const url = "http://localhost:8899/api/v1/user/register";
+
+  const [signupComplete, setSignupComplete] = useState(false);
 
   const [user, setUser] = useState({
     firstName: '',
@@ -17,6 +21,7 @@ function RegistrationForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     createUser();
+    setSignupComplete(true);
   };
   
   const createUser = async (props) => {
@@ -33,22 +38,25 @@ function RegistrationForm() {
   }
 
   return (<div>
-    <Container style={{width: 400}}>
-            <Form handleSubmit={handleSubmit}>
-            <Form.Input fluid label='First name' placeholder='Required' value={user.firstName} name='firstName' onChange={handleChange} />
-            <Form.Input fluid label='Middle names' placeholder='Optional' name='middleName' onChange={handleChange}/>
-            <Form.Input fluid label='Surname' placeholder='Required' value={user.lastName} name='lastName' onChange={handleChange} />
-              <Form.Field>
-                  <label>Username</label>
-                  <input placeholder='Email (Required)' type='email' value={user.email} name='email' onChange={handleChange} />
-              </Form.Field>
-              <Form.Field>
-                  <label>Password</label>
-                  <input placeholder='Required' type='password' value={user.password} name='password' onChange={handleChange} />
-              </Form.Field>
-              <Button onClick={handleSubmit} type='submit'>Sign up</Button>
-          </Form>
+    <Segment>
+      <Container style={{width: 400}}>
+              {!signupComplete && <Form handleSubmit={handleSubmit}>
+              <Form.Input fluid label='First name' placeholder='Required' value={user.firstName} name='firstName' onChange={handleChange} />
+              <Form.Input fluid label='Middle names' placeholder='Optional' name='middleName' onChange={handleChange}/>
+              <Form.Input fluid label='Surname' placeholder='Required' value={user.lastName} name='lastName' onChange={handleChange} />
+                <Form.Field>
+                    <label>Username</label>
+                    <input placeholder='Email (Required)' type='email' value={user.email} name='email' onChange={handleChange} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input placeholder='Required' type='password' value={user.password} name='password' onChange={handleChange} />
+                </Form.Field>
+                <Button onClick={handleSubmit} type='submit'>Sign up</Button>
+            </Form>}
+            {signupComplete && <RegistrationSuccess email={user.email} />}
         </Container>
+    </Segment>
   </div>);
 }
 
--- src/components/RegistrationSuccess.jsx
+++ src/components/RegistrationSuccess.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Grid,  } from 'semantic-ui-react';
 
-function RegistrationSuccess() {
-    return (<div>Registraction successful</div>);
+function RegistrationSuccess({ email }) {
+    return (<div>
+                Registration successful! <br/> 
+                An email verification link has been sent to your email address <b>{email}</b>.
+            </div>);
 }
 
 export default RegistrationSuccess;
